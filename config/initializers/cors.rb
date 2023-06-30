db3968ca0bf1c7aded010a96aa2bb10268c82364
jayastronomic <juliansmith94@hotmail.com>
2023-06-30 10:53:17 -0500

    updated Gemfile.lock

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://taylorstiki25th.rsvp", "https://tiki-taylor.web.app"
 
     resource "*",
       headers: :any,
