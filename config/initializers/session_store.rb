6a3b05c96ad373fd4fb7f266925e082ca1e03763
jayastronomic <juliansmith94@hotmail.com>
2023-07-01 22:46:01 -0500

    Edit Session Store Production Domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_tiki_taylor', domain: 'taylorstiki25th.rsvp'
+  Rails.application.config.session_store :cookie_store, key: '_tiki_taylor', domain: 'https://taylorstiki25th.rsvp'
 else
   Rails.application.config.session_store :cookie_store, key: '_tiki_taylor' 
 end
