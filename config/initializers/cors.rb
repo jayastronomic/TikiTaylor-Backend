b0f140a524dd3a465c32912a2afefc3b602d611e
jayastronomic <juliansmith94@hotmail.com>
2023-07-01 22:27:47 -0500

    Edit Session Store Production Domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://taylorstiki25th.rsvp", "https://tiki-taylor.web.app"
+    origins "https://taylorstiki25th.rsvp", "https://tiki-taylor.web.app", "http://localhost:3000"
 
     resource "*",
       headers: :any,
